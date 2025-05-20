3db71970b473ebd4e529a5c21f8db51dd6f6a392
José Antonio Villodres Zafra <148753281+JOSEANTONIOVZ123@users.noreply.github.com>
2025-05-20 11:44:30 +0200

    getByID

--- src/routes/film_routes.js
+++ src/routes/film_routes.js
@@ -14,6 +14,30 @@ ruta.get('/getFilms' , (req, res) => {
     }));
 });
 
+ruta.get('/getFilms/:id'), (req, res )=>{
+  const resultado = filmService.getFilmById(req.params.id);
+
+  resultado
+    .then((film) => {
+      if (!film) {
+        return res.status(400).json({
+          errorNumber: 400,
+          message: 'Película no encontrada',
+        });
+      }
+      res.status(200).json(film);
+    })
+    .catch((err) =>
+      res.status(500).json({
+        errorNumber: 500,
+        message: 'Error al buscar la película: ' + err,
+      })
+    );
+  
+
+
+} // id = _id de mongo
+
 
 ruta.post('/newFilm' , (req, res) => {
   const resultado = filmService.newFilm(req.body);
@@ -30,3 +54,4 @@ ruta.post('/newFilm' , (req, res) => {
       message: 'Error al intentar guardar la nueva película' + err
     }));
 });
+
