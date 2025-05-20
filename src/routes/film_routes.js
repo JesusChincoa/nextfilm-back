2aafbe082392d27fa9113b83ebeaedd57b3a4d92
David Sarmiento <dsar@uma.es>
2025-05-20 09:19:20 +0200

    NewFilm Function
    Added the new fuilm endpoint to the API

--- src/routes/film_routes.js
+++ src/routes/film_routes.js
@@ -2,25 +2,28 @@ const express = require('express');
 
 const filmService = require('../services/film_service');
 
-const ruta = express.Router();
+export const ruta = express.Router();
 
-const schema = Joi.object({
- 
-  titulo: Joi.string().required().min(3).max(30),
-  descripcion: Joi.string().min(3).max(30),
-  genero: Joi.string().required(),
-  estreno: Joi.number(),
-  director: Joi.string(),
-  duracion: Joi.number().min(0),
-  stock: Joi.number().min(0),
-  precio_alquiler: Joi.number().min(0),
-  
+ruta.get('/getFilms' , (req, res) => {
+  const resultado = filmService.getFilms();
+  resultado
+    .then((films) => res.status(200).json(films))
+    .catch((err) => res.status(400).json({ error: err }));
 });
 
 
-ruta.get("/getFilms" , (req, res) => {
-  const resultado = filmService.getFilms();
+ruta.post('/newFilm' , (req, res) => {
+  const resultado = filmService.newFilm(req.body);
+
+  if(resultado == null) return res.status(400).json({
+    errorNumber: 400,
+    message: 'Datos de película introducidos son incorrectos'
+  });
+
   resultado
-    .then((films) => res.status(200).json(films));
-    // .catch((err) => res.status(400).json({ error: err }));
+    .then((films) => res.status(200).json(films))
+    .catch((err) => res.status(500).json({
+      errorNumber: 500,
+      message: 'Error al intentar guardar la nueva película'
+    }));
 });
--- src/services/film_service.js
+++ src/services/film_service.js
@@ -9,11 +9,39 @@ const schema = Joi.object({
   estreno: Joi.number(),
   director: Joi.string(),
   duracion: Joi.number().min(0),
-  stock: Joi.number().min(0),
-  precio_alquiler: Joi.number().min(0),
-  
+  stock: Joi.number().required().min(0),
+  precio_alquiler: Joi.number().required().min(0),
 });
 
 export async function getFilms() {
     return await Film.find();
+}
+
+export async function newFilm(body){
+  const {error} = schema.validate({
+    titulo: body.titulo,
+    descripcion: body.descripcion,
+    genero: body.genero,
+    estreno: body.estreno,
+    director: body.director,
+    duracion: body.duracion,
+    stock: body.stock,
+    precio_alquiler: body.precio_alquiler,
+  });
+
+  if(error ) return null;
+
+  const film = new Film({
+    titulo: body.titulo,
+    descripcion: body.descripcion,
+    genero: body.genero,
+    estreno: body.estreno,
+    director: body.director,
+    duracion: body.duracion,
+    stock: body.stock,
+    precio_alquiler: body.precio_alquiler,
+  });
+
+
+  return await film.save();
 }
