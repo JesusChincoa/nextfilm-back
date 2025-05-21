5e4bb0e22f0f52974cdf5ef9badc1b12776ff21c
David Sarmiento <dsar@uma.es>
2025-05-21 11:48:35 +0200

    minor update on app

--- src/app.js
+++ src/app.js
@@ -1,29 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const films = require("./routes/film_routes");
+const seed = require("./routes/seed_route");
+const users = require("./routes/user_routes");
+const cors = require("cors");
 
-const express = require('express');
-const mongoose = require('mongoose')
-const films = require('./routes/film_routes')
-const seed = require('./routes/seed_route')
-const users = require('./routes/user_routes')
-const cors = require('cors')
+mongoose
+  .connect("mongodb://localhost:27017/nextfilm")
+  .then(() => console.log("Connecting to MongoDB"))
+  .catch((err) => console.error("error connecting MongoDB", err));
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: "http://localhost:4200", // a modificar mas tarde
+  })
+);
+
+app.use("/api/auth", users);
+app.use("/api", films);
+app.use("/api/database", seed);
 
 
-
-mongoose.connect('mongodb://localhost:27017/nextfilm')
-    .then(() => console.log('Connecting to MongoDB'))
-    .catch(err => console.error('error connecting MongoDB', err))
-
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended: true}));
-app.use('/api/auth', users);
- app.use('/api', films);
- app.use('/api/database', seed);
-
- app.use(cors({
-  origin: 'http://localhost:4200', // a modificar mas tarde 
-}));
-
-app.listen(3000, () =>{
-    console.log('API Express Listening...')
-
-})
+app.listen(3000, () => {
+  console.log("API Express Listening...");
+});
