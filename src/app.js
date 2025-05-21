045eb3ab05f4b3879cc6951ba47180ca5fd17bd9
David Sarmiento <dsar@uma.es>
2025-05-21 11:44:45 +0200

    updated app
    Added cors

--- src/app.js
+++ src/app.js
@@ -1,18 +1,29 @@
-const express = require('express')
+
+const express = require('express');
 const mongoose = require('mongoose')
 const films = require('./routes/film_routes')
+const seed = require('./routes/seed_route')
 const users = require('./routes/user_routes')
+const cors = require('cors')
+
+
 
 mongoose.connect('mongodb://localhost:27017/nextfilm')
-    .then(() => console.log('Conectado a MongoDB'))
-    .catch(err => console.error('No se pudo conectar a MongoDB', err))
+    .then(() => console.log('Connecting to MongoDB'))
+    .catch(err => console.error('error connecting MongoDB', err))
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-// app.use('/api/auth', users) TODO: Desde las rutas, poner el module.exports para acceder a ellas desde app
-// app.use('/api/films', films)
+ app.use('/api/auth', users)
+ app.use('/api', films)
+ app.use('/api/database', seed);
+
+ app.use(cors({
+  origin: 'http://localhost:4200', // a modificar mas tarde 
+}));
 
 app.listen(3000, () =>{
-    console.log('API Express escuchando...')
+    console.log('API Express Listening...')
+
 })
